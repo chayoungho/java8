64323e3f29ab1560c005a0b9f3ea230134f87867
yn-s07 <yn-s07@DESKTOP-46DUDQC>
2020-03-16 15:14:12 +0900

    3일차 증감 연산자

---
+++ java8/src/java8/Day0308.java
@@ -0,0 +1,55 @@
+package java8;
+
+public class Day0308 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		System.out.println();
+		
+		
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		//a가 앞에 있으면 전위 or 전치 연산자;
+		
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);  
+		//a가 뒤에 있으면 후위 or 후치 연산자;
+		
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		
+		a = 10;
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		
+		System.out.println(a);
+	}
+}
