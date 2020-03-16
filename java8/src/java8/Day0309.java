84903da81b454f693477600f5d082dcb511ac97b
yn-s07 <yn-s07@DESKTOP-46DUDQC>
2020-03-16 15:25:38 +0900

    3일차 삼항연산자(조건연산자)

---
+++ java8/src/java8/Day0309.java
@@ -0,0 +1,16 @@
+package java8;
+
+public class Day0309 {
+
+	public static void main(String[] args) {
+		int daegu =2_000_000;
+		int global = 100_0000;
+		
+		System.out.println(daegu >= global?"대구광역시":"대구시");
+		
+		
+		
+		
+	}
+
+}
