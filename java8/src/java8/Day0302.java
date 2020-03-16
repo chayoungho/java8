8bdba8f24a0f08b65c4a573d12b2d7cb1845b98c
yn-s07 <yn-s07@DESKTOP-46DUDQC>
2020-03-16 13:44:36 +0900

    3일차 산수연산자

--- java8/src/java8/Day0302.java
+++ java8/src/java8/Day0302.java
@@ -3,7 +3,20 @@ package java8;
 public class Day0302 {
 
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
+		/*
+		 * 기본 자료형은 각각을 정의하는 클래스가 존재합니다.
+		 * bute -> Byte
+		 * short -> Short
+		 * int -> Integer
+		 * long -> Long
+		 * float -> Float
+		 * double -> Double
+		 * char -> Character
+		 * 
+		 * 각각의 클래스에는 상수로 MAX_VALUE와 MIN_VALUE가 존재합니다.
+		 */
+		
+		
 
 	}
 
